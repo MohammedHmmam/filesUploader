7eb98b0c824c40fff7c1b57696fae18431880d05
Hammam <mohammedhmmam@gmail.com>
2022-10-12 20:32:45 +0200

    Add Directory separator in conf.php

--- conf.php
+++ conf.php
@@ -9,7 +9,7 @@
 
 
 //main download directory
-const UPLOADED_FILES = 'uploaded_files/';
+const UPLOADED_FILES = 'uploaded_files'.DIRECTORY_SEPARATOR;
 
 if(!file_exists(UPLOADED_FILES)){
     mkdir('uploaded_files',0777);
@@ -17,21 +17,21 @@ if(!file_exists(UPLOADED_FILES)){
 
 
 //images directory
-const IMAGES_PATH = UPLOADED_FILES.'images/';
+const IMAGES_PATH = UPLOADED_FILES.'images'.DIRECTORY_SEPARATOR;
 
 if(!file_exists(IMAGES_PATH)){
     mkdir(IMAGES_PATH,0777);
 }
 
 //PDF Directory
-const PDF_PATH = UPLOADED_FILES . 'pdf/';
+const PDF_PATH = UPLOADED_FILES . 'pdf'.DIRECTORY_SEPARATOR;
 
 if(!file_exists(PDF_PATH)){
     mkdir(PDF_PATH,0777);
 }
 
 //Excel sheets directory
-const EXCEL_PATH = UPLOADED_FILES . 'excelsheet/';
+const EXCEL_PATH = UPLOADED_FILES . 'excelsheet'.DIRECTORY_SEPARATOR;
 
 if(!file_exists(EXCEL_PATH)){
     mkdir(EXCEL_PATH,0777);
